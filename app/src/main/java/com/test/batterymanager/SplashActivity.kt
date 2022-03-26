1306f3542f116a65fc3b112e712ccca2b95cd289
Mohammad Yazdi <mohammadyt1379@gmail.com>
2022-03-26 21:05:00 +0430

    update ui activity_splash.xml

--- app/src/main/java/com/test/batterymanager/SplashActivity.kt
+++ app/src/main/java/com/test/batterymanager/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.test.batterymanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        gotoMainActivity();
+    }
+
+    private fun gotoMainActivity(){
+
+        Timer().schedule(timerTask {
+
+            startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+            finish()
+        },3000)
+
     }
 }
